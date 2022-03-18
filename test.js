3f934e47eb7355c8c9acdc3ad632b8da3544c983
mkmohitsharma85 <72628960+mkmohitsharma85@users.noreply.github.com>
2022-03-18 19:07:53 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,19 @@
+function GetP1Value(){
+    
+        var PValue = document.getElementById("demo");
+
+        if (PValue.style.display === "block") {
+            PValue.style.display = "none";
+          } else {
+            PValue.style.display = "block";
+          }
+}
+function GetP2Value(){
+    var PValue = document.getElementById("demo1");
+
+  if (PValue.style.display === "block") {
+    PValue.style.display = "none";
+  } else {
+    PValue.style.display = "block";
+  }
+}
